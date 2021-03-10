9894f0f4e4db3d26fb03b11ad5c7b8c33f6dd332
ChechG <2100@holbertonschool.com>
2021-03-10 12:49:23 +0000

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,7 +8,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t num = 0;
 
-	if (tree->parent == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
 	if (tree->parent->right != NULL || tree->parent->left != NULL)
 	{
